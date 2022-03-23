e495fa1c631c4903e092be1b457baeb69ddd92cb
barbodmb <mehdi.behroozi.mb@gmail.com>
2022-03-23 15:28:04 +0430

    validation

---
+++ DevCard_Project/DevCard_MVC/Controllers/TestController.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard_MVC.Controllers
+{
+    public class TestController : Controller
+    {
+    //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //public FileResult Index()
+        //{
+        //    var fileByte = System.IO.File.ReadAllBytes("wwwroot/test.txt");
+        //    const string fileName = "testFile.txt";
+        //    return File(fileByte, MediaTypeNames.Text.Plain, fileName);
+        //}
+
+        public javascriptResult Index()
+        {
+            return new javascriptResult("alert('salam mehdi behroozi hasta");
+        }
+    }
+
+    public class javascriptResult : ContentResult
+    {
+        public javascriptResult(string data)
+        {
+            Content = data;
+            ContentType = "application/javascript";
+        }
+    }
+}
